a22d4559aaf7f1e80ee7dd71eb0eee97619cc819
gabriel61 <sogabris@gmail.com>
2021-12-16 01:24:06 -0300

    Firt commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+
+const App = () => {
+
+  const message = "holy s";
+
+  return <div className="container">{message}</div>
+};
+
+export default App;
