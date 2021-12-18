e8dc54771f5dd8762a07b9befade3f82b91699d9
gabriel61 <sogabris@gmail.com>
2021-12-18 03:18:59 -0300

    Adiciona Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 
 import Header from "./components/Header"
 import './App.css';
@@ -54,17 +55,22 @@ const App = () => {
   }
 
   return (
-    <>
+    <Router>
       <div className="container">
         <Header/>
-        <AddTask handleTaskAddition={handleTaskAddition} />
-        <Tasks 
-          tasks={tasks} 
-          handleTaskClick={handleTaskClick} 
-          handleTaskDeletion={handleTaskDeletion}
+        <Route path="/" exact render={() => (
+          <>
+            <AddTask handleTaskAddition={handleTaskAddition} />
+            <Tasks 
+              tasks={tasks} 
+              handleTaskClick={handleTaskClick} 
+              handleTaskDeletion={handleTaskDeletion}
+            /> 
+          </>
+        )}
         />
       </div>
-    </>
+    </Router>
   );
 };
 
