479b3e08bd611fe74a501cca8fbf539f7e1f6d2f
gabriel61 <sogabris@gmail.com>
2021-12-21 20:14:05 -0300

    Add useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import { v4 as uuidv4 } from 'uuid';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
@@ -23,6 +24,17 @@ const App = () => {
       completed: true,
     }
   ]);
+
+  /* useEffect executa o bloco sempre que uma variavel muda */
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const { data } = await axios.get(
+        "https://jsonplaceholder.cypress.io/todos?_limit=10"
+      );
+      setTasks(data);
+    };
+    fetchTasks();
+  }, []);
 
   const handleTaskClick = (taskId) => {
     /* faz um map em cada task e se o id da task atual for igual
